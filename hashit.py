623dfdca1423efc649a0e4b45532a38a61cdac76
Jerry <winchel72@gmail.com>
2023-05-21 09:35:24 -0400

    main

--- hashit.py
+++ hashit.py
@@ -1,3 +1,25 @@
+import hashlib
+
+def MD5(filename: str) -> str:
+    
+    #filename = input("Enter the file name: ")
+    hash = hashlib.md5()
+    with open(filename,"rb") as f:
+        # Read and update hash in chunks of 4K
+        for byte_block in iter(lambda: f.read(4096),b""):
+            hash.update(byte_block)
+    return hash.hexdigest()
+
+def SHA256(filename: str) -> str:
+    
+    #filename = input("Enter the file name: ")
+    hash = hashlib.sha256()
+    with open(filename,"rb") as f:
+        # Read and update hash in chunks of 4K
+        for byte_block in iter(lambda: f.read(4096),b""):
+            hash.update(byte_block)
+    return hash.hexdigest()
+
 
 def main():
     print('Hello World!')
